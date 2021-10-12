3abbc5a67b64cbec0dee6d4e0498ab338043b0b6
Mike Gafert <mike@gafert.it>
2021-10-12 15:51:08 +0200

    uebungen fertig

---
+++ ErsteSchritteInJS/event.js
@@ -0,0 +1,10 @@
+function changeColorOnMouseOut()
+{
+    let control = document.getElementById("txbName");
+    control.style.backgroundColor = 'black';
+    control.style.color = 'white';
+
+    let control = document.getElementById('btn');
+    control.style.backgroundColor = 'red';
+    control.style.color = 'blue';
+}
